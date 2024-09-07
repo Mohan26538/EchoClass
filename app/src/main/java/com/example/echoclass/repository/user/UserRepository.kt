2f6a5eee72e7ed49e270f7cf8b77f6cb179d14df
MOHANKUMAR R <157295367+Mohan26538@users.noreply.github.com>
2024-09-07 16:09:13 +0530

    initail commit

--- app/src/main/java/com/example/echoclass/repository/user/UserRepository.kt
+++ app/src/main/java/com/example/echoclass/repository/user/UserRepository.kt
@@ -1,18 +1,44 @@
 package com.example.echoclass.repository.user
 
+import android.util.Log
 import com.example.echoclass.firebase.authentication.UserAuthentication
 import com.example.echoclass.model.User
+import com.google.firebase.Firebase
+import com.google.firebase.firestore.firestore
 
 class UserRepository():UserAuthentication {
+    private val firebaseAuth = Firebase.auth
+    private val db = Firebase.firestore
+
+
+
     override fun createUser(user: User, password: String, onSuccess: () -> Unit) {
-        TODO("Not yet implemented")
+        firebaseAuth.createUserWithEmailAndPassword(user.email,user.password)
+            .addOnSuccessListener{
+                val user = it.user?.let { it1 -> user.addId(it1.userId)}
+                if (user != null){
+                    saveUser(email,onSuccess)
+                }
+
+            }
+
     }
 
     override fun login(user: User, onSuccess: () -> Unit) {
-        TODO("Not yet implemented")
+        firebaseAuth.signInWithEmailAndPassword(email,password)
+            .addOnSuccessListener{
+                onSuccess()
+            }.addOnFailureListener{
+                Log.e("login",it.message.toString())
+            }
     }
 
     override fun saveUser(user: User, onSuccess: () -> Unit) {
-        TODO("Not yet implemented")
+        db.collection(USER_PATH).document(user.userId).set(user)
+            .addOnSuccessListener{
+                onSuccess()
+            }.addOnFailureListener{
+                Log.e("saveUser",it.message.toString())
+            }
     }
 }
